0e16675e30b9b72cdf1b4071c2bfcb5a5ad4c52e
ElsonJuniorDev <108693146+ElsonJuniorDev@users.noreply.github.com>
2023-10-06 13:16:59 -0300

    Projeto Finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,39 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(calcular){
     event.preventDefault();
 
-    const weigth = document.getElementById('weigth').value;
-    const heigth = (document.getElementById('heigth').value)/100;
+    const weigth = document.getElementById('weigth').value ;
+    const heigth = (document.getElementById('heigth').value); 
 
-    const imc = (weigth / ( heigth **2 )).toFixed(1);
-    console.log(weigth, heigth)
-    console.log(imc);
+    const imc = parseInt(weigth / ( heigth **2 )).toFixed(1);
+    
+    
+    
+    const value = document.getElementById('value');
+    let description = '';
+
+    value.classList.add('attention');
+
+    document.getElementById('imc-container').classList.remove('hidden');
+
+    if (imc < 18.5) {
+        description = 'Cuidado Você esta abaixo do peso !'
+    } 
+    else if (imc == 18.50 || imc <= 24.99) {
+        description = 'Seu peso esta Normal!'
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    }
+    else if (imc == 25 || imc <= 29.99) {
+        description = 'Você esta acima do peso!'
+    } 
+    else if (imc == 30 || imc <= 39.99) {
+        description = 'Você esta com Obesidade!'
+    } else  {
+        description = 'Voce esta com Obesidade Morbida'
+    }
+    
+
+    value.textContent = imc.replace(',', '.');
+    
+    document.getElementById('info-Result').textContent = description;
 });
